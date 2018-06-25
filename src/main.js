27fbe6e7ffb732e74b8cd8669d39e2ad8961d5c3
Dandy.Chen <ro1963855@gmail.com>
2018-06-25 21:43:16 +0800

    init bootstrap scss

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/stylesheet/build/all.min.css';
 import App from './App';
 import router from './router';
 
