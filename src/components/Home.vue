51f26112c3df02679efcfa9a6a8660dd19b9a8c4
Dandy.Chen <ro1963855@gmail.com>
2018-06-26 07:59:08 +0800

    adjust css and html naming

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,34 +8,41 @@
         </div>
       </div>
       <div class="row grid_height_1"></div>
-      <div class="row pg_first_row no-gutters">
-        <div class="col-4 pg_first_product">
-          <div class="pg_product_image" style="background-image: url('/static/img/1.jpg');"></div>
+      <div class="row grid_height_4 no-gutters">
+        <div class="col-4">
+          <div class="pg_product_image pg_480x400"
+                style="background-image: url('/static/img/1.jpg');"></div>
         </div>
-        <div class="col-4 pg_second_product d-flex align-items-center justify-content-center">
-          <div class="pg_product_image" style="background-image: url('/static/img/3.jpg');"></div>
+        <div class="col-4 d-flex align-items-center justify-content-center">
+          <div class="pg_product_image pg_240x200"
+                style="background-image: url('/static/img/3.jpg');"></div>
         </div>
       </div>
       <div class="grid_height_2"></div>
-      <div class="row pg_second_row no-gutters">
-        <div class="col-4 pg_three_product">
-          <div class="pg_product_image" style="background-image: url('/static/img/5.jpg');"></div>
+      <div class="row grid_height_4 no-gutters">
+        <div class="col-4">
+          <div class="pg_product_image pg_480x200"
+                style="background-image: url('/static/img/5.jpg');"></div>
         </div>
-        <div class="offset-2 col-2 pg_four_product">
-          <div class="pg_product_image" style="background-image: url('/static/img/7.jpg');"></div>
-        </div>
-      </div>
-      <div class="row pg_third_row no-gutters">
-        <div class="offset-2 col-2 pg_five_product">
-          <div class="pg_product_image" style="background-image: url('/static/img/6.jpg');"></div>
+        <div class="offset-2 col-2">
+          <div class="pg_product_image pg_240x400"
+                style="background-image: url('/static/img/7.jpg');"></div>
         </div>
       </div>
-      <div class="row pg_forth_row no-gutters">
-        <div class="col-2 pg_six_product d-flex align-items-end">
-          <div class="pg_product_image" style="background-image: url('/static/img/4.jpg');"></div>
+      <div class="row grid_height_2 no-gutters">
+        <div class="offset-2 col-2">
+          <div class="pg_product_image pg_240x200"
+                style="background-image: url('/static/img/6.jpg');"></div>
         </div>
-        <div class="offset-2 col-2 pg_seven_product">
-          <div class="pg_product_image" style="background-image: url('/static/img/2.jpg');"></div>
+      </div>
+      <div class="row grid_height_6 no-gutters">
+        <div class="col-2 d-flex align-items-end">
+          <div class="pg_product_image pg_240x400"
+                style="background-image: url('/static/img/4.jpg');"></div>
+        </div>
+        <div class="offset-2 col-2">
+          <div class="pg_product_image pg_480x400"
+                style="background-image: url('/static/img/2.jpg');"></div>
         </div>
       </div>
     </div>
