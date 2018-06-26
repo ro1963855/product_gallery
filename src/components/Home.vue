39f92e125f3e45cfa272a9928b56676bb1655761
Dandy <dandy.chen@advantech.com.tw>
2018-06-26 10:49:38 +0800

    finish home page background decorate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,39 +10,53 @@
       <div class="row grid_height_1"></div>
       <div class="row grid_height_4 no-gutters">
         <div class="col-4">
-          <div class="pg_product_image pg_480x400"
+          <div class="pg_product_image pg_480x400 position-relative"
                 style="background-image: url('/static/img/1.jpg');"></div>
+          <div class="pg_hollow_squre pg_240x200 pg_decorate_1"></div>
+          <div class="pg_stripe_squre pg_240x100 pg_decorate_2"></div>
         </div>
-        <div class="col-4 d-flex align-items-center justify-content-center">
+        <div class="col-4 d-flex align-items-center justify-content-center position-relative">
           <div class="pg_product_image pg_240x200"
                 style="background-image: url('/static/img/3.jpg');"></div>
+          <div class="pg_fill_squre pg_240x200 pg_decorate_3"></div>
+          <div class="pg_hollow_squre pg_240x200 pg_decorate_4"></div>
         </div>
       </div>
       <div class="grid_height_2"></div>
       <div class="row grid_height_4 no-gutters">
         <div class="col-4">
-          <div class="pg_product_image pg_480x200"
+          <div class="pg_product_image pg_480x200 position-relative"
                 style="background-image: url('/static/img/5.jpg');"></div>
+          <div class="pg_fill_squre pg_240x200 pg_decorate_5"></div>
+          <div class="pg_hollow_squre pg_240x200 pg_decorate_6"></div>
         </div>
         <div class="offset-2 col-2">
-          <div class="pg_product_image pg_240x400"
+          <div class="pg_product_image pg_240x400 position-relative"
                 style="background-image: url('/static/img/7.jpg');"></div>
+          <div class="pg_hollow_squre pg_240x200 pg_decorate_7"></div>
+          <div class="pg_stripe_squre pg_240x100 pg_decorate_8"></div>
         </div>
       </div>
       <div class="row grid_height_2 no-gutters">
         <div class="offset-2 col-2">
-          <div class="pg_product_image pg_240x200"
+          <div class="pg_product_image pg_240x200 pg_image_reverse position-relative"
                 style="background-image: url('/static/img/6.jpg');"></div>
+          <div class="pg_stripe_squre pg_240x100 pg_decorate_9"></div>
+          <div class="pg_hollow_squre pg_240x100 pg_decorate_10"></div>
         </div>
       </div>
       <div class="row grid_height_6 no-gutters">
         <div class="col-2 d-flex align-items-end">
-          <div class="pg_product_image pg_240x400"
+          <div class="pg_product_image pg_240x400 position-relative"
                 style="background-image: url('/static/img/4.jpg');"></div>
+          <div class="pg_hollow_squre pg_240x400 pg_decorate_11"></div>
+          <div class="pg_fill_squre pg_240x200 pg_decorate_12"></div>
         </div>
         <div class="offset-2 col-2">
-          <div class="pg_product_image pg_480x400"
+          <div class="pg_product_image pg_480x400 position-relative"
                 style="background-image: url('/static/img/2.jpg');"></div>
+          <div class="pg_hollow_squre pg_480x200 pg_decorate_13"></div>
+          <div class="pg_fill_squre pg_240x200 pg_decorate_14"></div>
         </div>
       </div>
     </div>
