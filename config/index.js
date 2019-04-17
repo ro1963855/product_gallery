c0de288d1e2fd10c50247ff0841366e2d7df9ca9
Dandy.Chen <Dandy.Chen@advantech.com.tw>
2019-04-17 14:32:24 +0800

    for Heroku deploy

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue/product_gallery/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
